f4f682cd8fb86c25462babe23cf63b278c731900
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-17 12:45:33 -0400

    user can sign up for the app

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,17 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    dobA = user_params[:dob].split('/')
+    bdate = Date.new(dobA[2].to_i, dobA[0].to_i, dobA[1].to_i)
+    user.dob = bdate
     if user.save
       token = encode_token({user_id: user.id})
-      json = {token: token, success: true, user: user}
+      json = {token: token, success: true, user: {id: user.id,
+                                                  name: user.name,
+                                                  email: user.email,
+                                                  user_type: user.user_type}}
     else
+      byebug
       json = {errors: "something went wrong.", success: false}
     end
     render json: json
