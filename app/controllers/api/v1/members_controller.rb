7239aa084ef469f3e455154a6fba251855308b47
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-20 12:33:04 -0400

    add create

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -5,7 +5,17 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def create
-
+    new_member = Member.new(member_params)
+    dobA = member_params[:dob].split('/')
+    bdate = Date.new(dobA[2].to_i, dobA[0].to_i, dobA[1].to_i)
+    new_member.dob = bdate
+    new_member.register_date = Date.today
+    if new_member.save
+      json = {success: true}
+    else
+      json = {success: false}
+    end
+    render json: json
   end
 
   def update
@@ -19,6 +29,7 @@ class Api::V1::MembersController < ApplicationController
   private
 
   def member_params
-    params.require(:member).permit(:name, :email, :phone_number, :gender, :dob, :register_date, :active, :info, :welcome_mail)
+    params.require(:member).permit(:name, :email, :phone_number, :gender, :dob, :active, :info, :welcome_mail)
+    # params.require(:member).permit(:name, :email, :phone_number, :gender, :dob, :register_date, :active, :info, :welcome_mail)
   end
 end
