ab9a60dcc4e3b9bdf4557428be755a5aceb59fff
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-29 11:44:38 -0400

    add update date for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone_number, :gender, :dob, :active, :user_type, :groups
+  attributes :id, :name, :email, :phone_number, :gender, :dob, :active, :user_type, :groups, :updated_at
 end
